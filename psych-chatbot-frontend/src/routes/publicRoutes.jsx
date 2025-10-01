2f8286ca951799a12ec39e923c1e2d65c0a3a02c
AnhDucNgo1245 <thefastestmanal@gmail.com>
2025-10-01 16:43:58 +0700

    40% UI BlogPage

--- psych-chatbot-frontend/src/routes/publicRoutes.jsx
+++ psych-chatbot-frontend/src/routes/publicRoutes.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Home from '../pages/Home/Home'
-import AboutPage from '../pages/About/AboutPage'
+import React from "react";
+import Home from "../pages/Home/Home";
+import AboutPage from "../pages/About/AboutPage";
+import Blog from "../pages/Blog/Blog";
 
 export const publicRoutes = [
-    {
-        path: '/',
-        element: <Home/>
-    },
-    {
-        path: '/about',
-        element: <AboutPage/>
-    }
-]
+  {
+    path: "*",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
+  },
+];

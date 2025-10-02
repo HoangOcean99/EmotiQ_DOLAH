24541446f05fe0b0313c756bf5755e785b0c4892
AnhDucNgo1245 <thefastestmanal@gmail.com>
2025-10-02 11:10:07 +0700

    fix App, AppRouter

--- psych-chatbot-frontend/src/App.jsx
+++ psych-chatbot-frontend/src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import Home from './pages/Home/Home'
-import AboutPage from './pages/About/AboutPage'
-import Login from './pages/Login/login'
-import MainChat from './pages/MainChat/MainChat'
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Blog from './pages/Blog'
-import ContactUs from './pages/ContactUs'
-import ProtectedRoute from './components/protected/protected'
+import { useState } from "react";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/homePage' element={<AboutPage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/blog' element={<Blog />} />
-        <Route path='/contactus' element={<ContactUs />} />
-        <Route path='/mainChat' element={
-          <ProtectedRoute>
-            <MainChat />
-          </ProtectedRoute>
-        } />
-
-      </Routes>
-    </BrowserRouter>
-  )
+    <>
+      <AppRouter />
+    </>
+  );
 }
 
-export default App
+export default App;
--- psych-chatbot-frontend/src/routes/publicRoutes.jsx
+++ psych-chatbot-frontend/src/routes/publicRoutes.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Home from "../pages/Home/Home";
 import AboutPage from "../pages/About/AboutPage";
 import Blog from "../pages/Blog/Blog";
+import ContactUs from "../pages/ContactUs/ContactUs";
+import Login from "../pages/Login/Login";
+import ProtectedRoute from "../components/protected/protected";
+import MainChat from "../pages/MainChat/MainChat";
 
 export const publicRoutes = [
   {
@@ -16,4 +20,18 @@ export const publicRoutes = [
     path: "/blog",
     element: <Blog />,
   },
+  {
+    path: "/contactus",
+    element: <ContactUs />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/mainChat",
+    element: <ProtectedRoute>
+      <MainChat />
+    </ProtectedRoute>
+  }
 ];
